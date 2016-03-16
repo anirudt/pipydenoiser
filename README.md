5ad6ff9e22e259f8aac11d304a53963447490c5e
anirudt <anirudt@gmail.com>
2016-03-16 10:51:41 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##What?
+Python audio filtering module.
+
+##TODO:
+* Add a graphing structure
+* Analyse the noise
---
+++ proc.py
@@ -0,0 +1,5 @@
+from pydub import AudioSegment
+import pdb
+
+song = AudioSegment.from_wav('records/NC131.wav')
+pdb.set_trace()
