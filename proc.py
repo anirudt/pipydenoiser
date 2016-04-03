1b88213eee57c6d40312d7371f446d89d1484831
anirudt <anirudt@gmail.com>
2016-04-03 08:32:51 +0530

    added core functionality, and comments

--- proc.py
+++ proc.py
@@ -5,29 +5,39 @@ import pdb
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
 import wave
-# TODO: Import the auto-record module, and 
-# record the sounds with and without signal.
+import cmath, math
 
+#################################################################################################
 # IDEAL FILTER GETTER
 def load_ideal_filter(opt, N):
+    H = []
     if opt == '1':
         for i in range(N):
-            if i >= N//2-300 && i <= N//2-15:
+            if i > (N//2-300) and i < (N//2-15):
                 H.append(1)
-            if i >= N//2+15 && i <= N/2+300:
+                continue
+            if i > (N//2+15) and i < (N//2+300):
                 H.append(1)
+                continue
             else:
                 H.append(0)
     if opt == '2':
         for i in range(N):
-            if i >= N//2-1000 && i <= N//2-5:
+            if i >= N//2-1000 and i <= N//2-5:
                 H.append(1)
-            if i >= N//2+5 && i <= N/2+1000:
+                continue
+            if i >= N//2+5 and i <= N//2+1000:
                 H.append(1)
+                continue
             else:
                 H.append(0)
+    freq_axis = range(-N//2, N//2)
+    graphify_plot(freq_axis, H, "frequency axis", \
+            "amplitude", "Filter Plot", "filt")
     return H
 
+#################################################################################################
+# Getter Functions
 def get_SNR(X, Y):
     """ 
     Assuming that the output signal is pure signal, 
@@ -43,11 +53,11 @@ def get_SNR(X, Y):
     Py = sum(abs(Y)*abs(Y))
     return 20.0*math.log(Py/Px)
 
-# Getter Function
 def get_params():
-    """ Returns parameters of the audio file 
-        in the following order:
-        [nchannels, sampwidth, framerate, nframes, comptype, compname, samplerate]
+    """ 
+    Returns parameters of the audio file 
+    in the following order:
+    [nchannels, sampwidth, framerate, nframes, comptype, compname, samplerate]
     """
     f = wave.open('records/myvoice.wav', 'rb')
     [Fs, g] = scipy.io.wavfile.read('records/myvoice.wav') 
@@ -56,7 +66,8 @@ def get_params():
     list_p.append(Fs)
     return list_p
 
-# Helper Functions
+#################################################################################################
+# Helper Functions for plotting
 def graphify_plot(x, y, xlabel, ylabel, title, name, axis=None):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -78,32 +89,55 @@ def graphify_stem(x, y, xlabel, ylabel, title, name, axis=None):
     plt.show()
     if axis != None:
         plt.axis(axis)
+
+#################################################################################################
+# Core Functionality
+def filter(X, s):
+    # Sample first half:
+    H = load_ideal_filter(s, len(X))
+    Y = [H[i]*X[i] for i in range(len(X))]
+    # pdb.set_trace()
+
+    # Assuming that it is fftshifted initially.
+    Yi = np.fft.ifftshift(Y)
+    y = ifft(Yi)
+    return y, Y
+
 def capture_bkgnd():
     sample_rate, bkgnd = scipy.io.wavfile.read('records/myvoice.wav')
+
+    # Takes the average of both channels
+    bkgnd = np.mean(bkgnd, axis=1)
+    t = range(len(bkgnd))
 
     a = get_params()
     Fs = a[-1]
     #pdb.set_trace()
     freq_resp = fft(bkgnd)
-    freq_resp = np.fft.fftshift(freq_resp)
+    freq_resp = list(np.fft.fftshift(freq_resp))
     freq_axis = list(np.arange(-Fs/2,Fs/2,Fs*1.0/len(bkgnd)))
     length = len(freq_resp)
     print len(freq_axis), len(freq_resp)
-    graphify_plot(freq_axis, abs(freq_resp), "frequency axis", \
-            "amplitude", "Voice Freq Plot", "freq_resp_signal")
+    graphify_plot(freq_axis, np.abs(freq_resp), "frequency axis", \
+            "amplitude", "Voice Freq Plot", "voice")
 
-    Y = filter(freq_resp, '1')
+    y, Y = filter(freq_resp, '2')
+    print "SNR is ", get_SNR(freq_resp, Y)
+    graphify_plot(freq_axis, np.abs(Y), "frequency axis", \
+            "amplitude", "Filtered Voice Freq Plot", "filt_voice_freq")
+    real_y = np.array([y[i].real for i in range(len(y))], dtype=np.int16)
+    graphify_plot(t, real_y, "Time axis", \
+            "amplitude", "Filtered Voice Time Plot", "filt_voice")
 
-def filter(X, s):
-    # Sample first half:
-    H = load_ideal_filter(s)
-    Y = [H[i]*X[i] for i in range(len(X))]
+    #TODO: Plot the real value of y versus time, and write to a 
+    # wav file, and play it.
+    # Reshaping the matrix
+    out = np.array(list(real_y)+list(real_y), dtype = np.int16)
+    out = out.reshape((len(bkgnd), 2))
+    pdb.set_trace()
+    scipy.io.wavfile.write('records/proc_myvoice.wav', sample_rate, out)
 
-    # Assuming that it is fftshifted initially.
-    Y = np.fft.ifftshift(Y)
-    y = np.ifft(Y)
-    return y
-
+# TODO: Enhance this module.
 def vol_add():
     f = AudioSegment.from_wav('records/myvoice.wav')
     f1 = f + 10
@@ -111,11 +145,7 @@ def vol_add():
 
 if __name__ == '__main__':
     # TODO: Auto-capture background noise signal
-    # This signal could be used as our noise estimate
     print get_params()
     capture_bkgnd()
-
+    # load_ideal_filter('1', 1000)
     # TODO: Auto-capture required signal
-
-    # TODO: Perform necessary processing.
-    #pdb.set_trace()
