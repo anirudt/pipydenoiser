2b9e141fedc7fdceef542c5c42ecb0d0371b2b5b
anirudt <anirudt@gmail.com>
2016-04-06 13:03:19 +0530

    fixed npint16 bug, and added TODOs

--- butterworth.py
+++ butterworth.py
@@ -12,18 +12,25 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     low = lowcut / nyq
     high = highcut / nyq
     b, a = butter(order, [low, high], btype='band')
+    # TODO: probably test the freq response of the
+    # filter formed, by using freqs command
     return b, a
 
 
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
+    # TODO: A debug print here, suggests that y has a lot of
+    # NAN values. Try to find out what is happening, or go 
+    # through this link: http://stackoverflow.com/questions/8811518/scipy-lfilter-returns-only-nans
     return y
     
 def main():
-    sample_rate, bkgnd = scipy.io.wavfile.read('a3.wav')
+    sample_rate, bkgnd = scipy.io.wavfile.read('records/a3.wav')
     bkgnd = np.mean(bkgnd, axis=1, dtype=np.int16)
     t = range(len(bkgnd))
-    sig = butter_bandpass_filter(bkgnd, 10, 2000, sample_rate, 5)
-    scipy.io.wavfile.write('out/a3.wav', sample_rate, sig.real)
-if __name__ == '__main__':main()
+    sig = np.array(butter_bandpass_filter(bkgnd, 10, 2000, sample_rate, 5), dtype=np.int16)
+    scipy.io.wavfile.write('records/butterworth.wav', sample_rate, sig.real)
+
+if __name__ == '__main__':
+    main()
