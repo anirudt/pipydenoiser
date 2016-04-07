9716a8769604d9e96e3cc08d966c214f0501fc8f
anirudt <anirudt@gmail.com>
2016-04-07 07:29:26 +0530

    butterworth working, needs vol_add()

--- butterworth.py
+++ butterworth.py
@@ -6,30 +6,46 @@ import wave
 import cmath, math
 import optparse
 from scipy.signal import butter, lfilter
+from scipy import signal
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
+def butter_bandpass(lowcut, highcut, fs, order=3):
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
     b, a = butter(order, [low, high], btype='band')
-    # TODO: probably test the freq response of the
-    # filter formed, by using freqs command
+    
+    #print np.all(np.abs(np.roots(a))<1), (low, high, order), np.max(np.roots(a))
     return b, a
 
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=3):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
-    # TODO: A debug print here, suggests that y has a lot of
-    # NAN values. Try to find out what is happening, or go 
-    # through this link: http://stackoverflow.com/questions/8811518/scipy-lfilter-returns-only-nans
+    w, h = signal.freqz(b, a)
+    Y = np.fft.fftshift(np.fft.fft(y))
+    freq_axis = (np.arange(-fs/2,fs/2,fs*1.0/len(data)))
+    plt.plot(freq_axis, abs(Y))
+    plt.show()
+    plt.plot(w, 20 * np.log10(abs(h)))
+    plt.show()
     return y
-    
+
+def stable_filter_finder(Fs):
+    low = [5, 10, 15, 20]
+    high = [1000, 1200, 1500, 2000]
+    order = [1,3,5,7,9,11,13,15]
+    fs = Fs
+    for l in low:
+        for h in high:
+            for o in order:
+                butter_bandpass(l, h, Fs, o)
+
 def main():
     sample_rate, bkgnd = scipy.io.wavfile.read('records/a3.wav')
     bkgnd = np.mean(bkgnd, axis=1, dtype=np.int16)
     t = range(len(bkgnd))
-    sig = np.array(butter_bandpass_filter(bkgnd, 10, 2000, sample_rate, 5), dtype=np.int16)
+    sig = np.array(butter_bandpass_filter(bkgnd, 10, 2000, sample_rate, 3), dtype=np.int16)
+    #stable_filter_finder(sample_rate)
     scipy.io.wavfile.write('records/butterworth.wav', sample_rate, sig.real)
 
 if __name__ == '__main__':
